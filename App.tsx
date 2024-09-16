0eb0f6647e056434cb797658e1fecc3fd38bf575
Badr Amine <badr.amine@merkle.com>
2024-09-16 02:05:57 +0200

    chore: Refactor App.tsx to use Redux and React Navigation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import type React from 'react';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { Provider } from 'react-redux';
+import { store } from './src/redux/store';
+import { BottomTabNavigator } from './src/navigation/BottomTabNavigator';
+import { useFonts } from './src/utils/fonts/Fonts';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
-export default function App() {
+
+export default () => {
+
+  const fontsLoaded = useFonts()
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <SafeAreaView style={styles.safeArea}><BottomTabNavigator /></SafeAreaView>
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  safeArea: {
+    flex: 1
   },
 });
